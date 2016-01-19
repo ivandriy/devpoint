0429a8901c6e83f10820caff1ea8cd8d6d3b35b5
Andrii Ivanskiy <andrii@ivanskiy.com>
2016-01-19 22:23:11 +0200

    Added Select-AzureSubscription

--- Start_VMs.ps1
+++ Start_VMs.ps1
@@ -1,11 +1,12 @@
 ﻿#Add-AzureAccount
-Set-AzureSubscription -SubscriptionName "Visual Studio Professional with MSDN"
+Set-AzureSubscription -SubscriptionName "Visual Studio Professional with MSDN" 
+Select-AzureSubscription -SubscriptionName "Visual Studio Professional with MSDN" -Current 
 $MossVm=Get-AzureVM -ServiceName LST-APP -Name LST-APP
 $SQLVm=Get-AzureVM -ServiceName LST-DB -Name LST-DB
 $DC = Get-AzureVM -ServiceName LST-DC1 -Name LST-DC1
 
 $DC|Start-AzureVM
-Start-Sleep -Seconds 120
+Start-Sleep -Seconds 600
 $SQLVm|Start-AzureVM 
-Start-Sleep -Seconds 120
+Start-Sleep -Seconds 300
 $MossVm|Start-AzureVM
--- Stop_VMs.ps1
+++ Stop_VMs.ps1
@@ -1,4 +1,5 @@
 ﻿Set-AzureSubscription -SubscriptionName "Visual Studio Professional with MSDN"
+Select-AzureSubscription -SubscriptionName "Visual Studio Professional with MSDN" -Current
 $MossVm=Get-AzureVM -ServiceName LST-APP -Name LST-APP
 $SQLVm=Get-AzureVM -ServiceName LST-DB -Name LST-DB
 $DC = Get-AzureVM -ServiceName LST-DC1 -Name LST-DC1
