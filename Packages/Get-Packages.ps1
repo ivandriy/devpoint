c8cfa6d990c2d9f2282da65b13fe0477483d8421
Andrii Ivanskiy <andrii@ivanskiy.com>
2016-09-08 21:28:32 +0300

    Added function to get installed software from registry

--- Packages/Get-Packages.ps1
+++ Packages/Get-Packages.ps1
@@ -1,5 +1,5 @@
 ﻿#Requires -Version 5.0
-#$PowerShellVersion = $PSVersionTable.PSVersion.Major
+
 
 ###########FUNCTIONS###############
 function Get-Hash
@@ -24,18 +24,84 @@ function Get-UninstallPath
     return $UninstallPath
 }
 
-############MAIN##############
+function Get-ProgramsFromRegistry
+{    
+    $ProgramsList = @()
+    $RegistryLocation = 'SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\',
+                        'SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\'
+    $RegBase = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine,$env:COMPUTERNAME)
+    foreach ($UninstallKey in $RegistryLocation)
+    {
+        if ($RegBase)
+        {
+            $CurrentRegKey = $RegBase.OpenSubKey($UninstallKey)
+            if($CurrentRegKey)
+            {
+                $Subkeys = $CurrentRegKey.GetSubKeyNames()
+                if($Subkeys)
+                {
+                    foreach($key in $Subkeys)
+                    {
+                        $ThisKey = $UninstallKey+"\\"+$key
+                        $ThisSubKey = $RegBase.OpenSubKey($ThisKey)
+                        $ProgramName = $ThisSubKey.GetValue("DisplayName")
+                        if([string]::IsNullOrWhiteSpace($ProgramName))                        
+                        {
+                            if(!($key.StartsWith('{')))
+                            {
+                                $ProgramName = $key
+                            }
+                            else
+                            {
+                             continue
+                            }
+                        }                        
+                        $ProgramsList += New-Object -TypeName psobject -Property @{
+                                DisplayName = $ProgramName
+                                DisplayVersion = $ThisSubKey.GetValue("DisplayVersion")
+                                DisplayIcon = $ThisSubKey.GetValue("DisplayIcon")                                
+                                InstallDate = $ThisSubKey.GetValue("InstallDate")
+                                InstallLocation = $ThisSubKey.GetValue("InstallLocation")
+                                InstallSource = $ThisSubKey.GetValue("InstallSource")
+                                Publisher = $ThisSubKey.GetValue("Publisher")
+                                UninstallString = $ThisSubKey.GetValue("UninstallString")
+                                PathToUninstall = Get-UninstallPath -InputString $ThisSubKey.GetValue("UninstallString")
+                            }
+                    }
+                }
+            }
+        }
+    }
 
-$programs=Get-Package -ProviderName Programs|Select-Object -Property Name,Version `
+    return $ProgramsList
+}
+############MAIN##############
+$InstalledPrograms = @()
+$PowerShellVersion = $PSVersionTable.PSVersion.Major
+if($PowerShellVersion -lt 5)
+{
+    $InstalledPrograms = Get-ProgramsFromRegistry
+}
+else
+{
+    $InstalledPrograms=Get-Package -ProviderName Programs|Select-Object -Property @{Name='DisplayName'; Expression={($_.Name)}} `
+                ,@{Name='DisplayVersion'; Expression={($_.Version)}} `
+                ,@{Name='DisplayIcon'; Expression={($_.Metadata['DisplayIcon'])}}`
+                ,@{Name='InstallDate'; Expression={($_.Metadata['InstallDate'])}}`
+                ,@{Name='InstallLocation'; Expression={($_.Metadata['InstallLocation'])}}`
+                ,@{Name='InstallSource'; Expression={($_.Source)}}`
+                ,@{Name='Publisher'; Expression={($_.Metadata['Publisher'])}}`
                 ,@{Name='UninstallCommand'; Expression={($_.Metadata['UninstallString'])}}`
-                ,@{Name='PathToUninstall';Expression={Get-UninstallPath -InputString $($_.Metadata['UninstallString']) }}                
+                ,@{Name='PathToUninstall';Expression={Get-UninstallPath -InputString $($_.Metadata['UninstallString']) }} 
+}
 
 $AllPrograms = @()
-foreach($program in $programs)
+foreach($program in $InstalledPrograms)
 {
     $md5hash = $null
     $sh1hash = $null
-    $sh256hash = $null    
+    $sh256hash = $null
+    $sign = $null    
     if($program.PathToUninstall -ne '')
     {                
         if((Test-Path $program.PathToUninstall -IsValid) -and (Test-Path $program.PathToUninstall))
@@ -47,8 +113,12 @@ foreach($program in $programs)
         }
     }
     $AllPrograms += New-Object -TypeName psobject -Property @{
-        Name = $program.Name
-        Version = $program.Version
+        DisplayName = $program.DisplayName
+        DisplayVersion = $program.DisplayVersion
+        DisplayIcon = $program.DisplayIcon
+        InstallDate = $program.InstallDate
+        InstallLocation = $program.InstallLocation
+        Publisher = $program.Publisher
         UninstallCommand = $program.UninstallCommand
         PathToUninstall = $program.PathToUninstall
         MD5Hash = $md5hash
@@ -58,4 +128,4 @@ foreach($program in $programs)
     }
 }
 
-$AllPrograms|Select-Object -Property Name,Version,UninstallCommand,PathToUninstall,MD5Hash,SH1Hash,SH256Hash,DigitalSignature|ConvertTo-Json
+$AllPrograms|Select-Object -Property DisplayName,DisplayVersion,DisplayIcon,InstallDate,InstallLocation,Publisher,UninstallCommand,PathToUninstall,MD5Hash,SH1Hash,SH256Hash,DigitalSignature|ConvertTo-Json
