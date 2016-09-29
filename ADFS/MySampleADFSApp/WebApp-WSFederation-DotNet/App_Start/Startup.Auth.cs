4c1b6771d51def5343510e5451ac8b7cd0208b06
Andrii Ivanskiy <andrii@ivanskiy.com>
2016-09-29 20:13:34 +0300

    Clean up unused staff

--- ADFS/MySampleADFSApp/WebApp-WSFederation-DotNet/App_Start/Startup.Auth.cs
+++ ADFS/MySampleADFSApp/WebApp-WSFederation-DotNet/App_Start/Startup.Auth.cs
@@ -34,22 +34,9 @@ namespace WebApp_WSFederation_DotNet
 {
     public partial class Startup
     {
-        //
-        // The Client ID is used by the application to uniquely identify itself to Azure AD.
-        // The Metadata Address is used by the application to retrieve the signing keys used by Azure AD.
-        // The AAD Instance is the instance of Azure, for example public Azure or Azure China.
-        // The Authority is the sign-in URL of the tenant.
-        // The Post Logout Redirect Uri is the URL where the user will be redirected after they sign out.
-        //
         private static string realm = ConfigurationManager.AppSettings["ida:RPIdentifier"];
         private static string metadata = string.Format("https://{0}/federationmetadata/2007-06/federationmetadata.xml", ConfigurationManager.AppSettings["ida:ADFS"]);
-        private static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
-        private static string tenant = ConfigurationManager.AppSettings["ida:Tenant"];
-        //private static string metadata = string.Format("{0}/{1}/federationmetadata/2007-06/federationmetadata.xml", aadInstance, tenant);
-       
-
-        string authority = String.Format(CultureInfo.InvariantCulture, aadInstance, tenant);
-
+               
         public void ConfigureAuth(IAppBuilder app)
         {
             app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
